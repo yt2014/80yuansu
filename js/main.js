471bd1b2e576df5f358cb73dd6ccf43a1e896f85
yt2014 <taoy_2007@126.com>
2015-05-14 22:42:05 +0800

    main frame
    construct the frame for main page

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	
+	//alert("ready height is "+$(document).innerHeight()*0.05);
+	$("#div_top").height($(document).innerHeight()*0.08);
+	$("#div_body").height($(document).innerHeight()*0.84);
+	$("#div_buttom").height($(document).innerHeight()*0.08);
+});
