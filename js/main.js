b7f60398d836304d95f4d8325be3ee4f28bda839
yt2014 <taoy_2007@126.com>
2015-06-01 17:47:02 +0800

    concepts adding

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 		function(e)
 		{
 		    e.preventDefault();
-
+                    $("#text_area").empty(); 
 		    $.ajax({
                               url: 'Company.txt',
                               dataType: 'text',
@@ -48,7 +48,77 @@ $(document).ready(function(){
 		
 		);/*end of Company_Background click*/
 
+       $("#Company_Concept").click(
+	       function (e)
+               {
 
+		    e.preventDefault();
+	            $("#text_area").empty();
+		    $("#text_area").css("width","100%");
+		    $("#text_area").css("height","100%");
+
+		    var text_left = $("<div class = \"div_subtext\"> </div>");
+		    var text_middle = $("<div class = \"div_subtext\"> </div>");
+		    var text_right = $("<div class = \"div_subtext\"> </div>");
+                    
+
+		    var concept1_div = $("<div> </div>");
+		    concept1_div.css("width","100%");
+
+                     $("#text_area").append(concept1_div);
+
+                    var concept1 = $("<p>以人为本,活力空间</p>");
+                    text_left.append(concept1);
+
+		    var concept1_content = $("<p></p>");
+		    text_right.append(concept1_content);
+
+                    text_left.css("width","20%");
+		    //text_left.css("height","100%");
+		    text_left.css("background-color","red");
+                    concept1_div.append(text_left);
+		  //  text_middle.insertAfter(text_left);
+		  //  text_right.insertAfter(text_middle);
+                    concept1_div.append(text_middle);
+                    concept1_div.append(text_right);
+
+                    $(".div_subtext").css("float","left");
+
+
+		    text_middle.css({"width":"3%","background-color":"yellow"});
+		    //text_middle.css("height","100%");
+		    //text_middle.css("background-color","yellow");
+
+		    text_right.css("width","67%");
+		    //text_right.css("height","100%");
+		    text_right.css("background-color","black");
+
+                   // alert("aaaaaa");
+                    var fso = new ActiveXObject("Scripting.FileSystemObject"); 
+                    alert(fso);
+                    var f = fso.OpenTextFile("..//concepts.txt",1); 
+
+		    alert(f);
+
+                   // var s = "";
+
+                 //   while (!f.AtEndOfStream)
+
+                    var s = f.ReadLine();
+		    alert(s);
+                    concept1.text(s);
+                    s=f.ReadLine();
+		    concept1_content.text(s);
+
+                    f.Close();
+
+                  //  return s; 
+		    
+
+	       }
+	       
+	       
+	       );
 
         /*navgators click events, set the left links*/
 	/*历史作品*/
